e762f762e4afe88362ebb3840bcc44c98b7c849a
tw <dbsxodnjs1@naver.com>
2016-09-22 10:20:49 +0900

    score add

---
+++ Assets/script/score.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class score : MonoBehaviour {
+
+	public float value = 0.0f;
+	private Text scoreValue;
+
+
+	void Awake(){
+
+		scoreValue = GetComponent<Text> ();
+	}
+
+
+	// Use this for initialization
+
+
+	void Start () {
+		
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+
+		scoreValue.text = string.Format ("{0:d1}", (int)value);
+
+
+
+	}
+}
