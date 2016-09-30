2dda3f76dafcf128f4acfd1133df82b604c2e5f3
taewon <gccerqw@nate.com>
2016-09-30 19:10:23 +0900

    button update

--- Assets/script/Move.cs
+++ Assets/script/Move.cs
@@ -3,20 +3,29 @@ using System.Collections;
 
 public class Move : MonoBehaviour {
 
+	leftButtonScript _left;
+	rightButtonScript _right;
+
 	// Use this for initialization
 	public void moveDown(float y, float z) {
+		_left = GameObject.FindObjectOfType<leftButtonScript>();
+		_right = GameObject.FindObjectOfType<rightButtonScript>();
 
 		this.transform.Translate(0.0f, y, z);
 	
 	}
 
 	public void moveLeft(float x, float y) {
+		_left = GameObject.FindObjectOfType<leftButtonScript>();
+		_right = GameObject.FindObjectOfType<rightButtonScript>();
 
 		this.transform.Translate(x, y, 0.0f);
 
 	}
 
 	public void moveRight(float x, float y) {
+		_left = GameObject.FindObjectOfType<leftButtonScript>();
+		_right = GameObject.FindObjectOfType<rightButtonScript>();
 
 		this.transform.Translate(x, y, 0.0f);
 
