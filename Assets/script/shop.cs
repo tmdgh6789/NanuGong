44b4d30134630eace8a638be86e985325690f7ff
Eun Ji <a33981027@gmail.com>
2016-10-03 18:56:31 +0900

    Scene/shop.unity add

---
+++ Assets/script/shop.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class shop : MonoBehaviour {
+
+	public void OnMouseDown() {
+        SceneManager.LoadScene(3);
+    }
+}
---
+++ Assets/script/shopBefore.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class shopBefore : MonoBehaviour {
+    public void OnMouseDown() {
+        SceneManager.LoadScene(1);
+    }
+}
